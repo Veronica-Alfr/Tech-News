47b0d687f66456179aa77b5027c0af1250ebb7e5
Veronica-Alfr <veagalves@gmail.com>
2022-11-10 12:47:34 -0300

    feat: testando função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -13,7 +13,7 @@ def search_by_title(title):
 def search_by_date(date):
     try:
         # formatted_date = datetime(date).strftime("%m/%d/%Y")
-        formatted_date = datetime.strptime(date, "%d/%m/%Y")
+        formatted_date = datetime.strptime(date, "%d-%m-%Y")
         news_list = search_news({"timestamp": formatted_date})
         title_and_url = [(news["title"], news["url"]) for news in news_list]
         return title_and_url
